1e7235e786299fc431e0fa3e08867352df664963
juanordone <juan.ordonez89@hotmail.es>
2023-02-09 13:51:25 +0100

    vistas

--- vite-project/src/views/Home/Home.jsx
+++ vite-project/src/views/Home/Home.jsx
@@ -1,5 +1,10 @@
+import Navbar from "../../components/Navbar/Navbar";
+
 export default function Home(){
     return(
+        <>
+        <Navbar/>
         <h1>ESTO ES LA HOME </h1>
+        </>
     )
 }
