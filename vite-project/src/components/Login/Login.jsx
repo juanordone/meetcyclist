42d7872cbf047a7d2afb615395769b94d3ed3864
juanordone <juan.ordonez89@hotmail.es>
2023-02-09 19:20:13 +0100

    cambios de nombre

--- vite-project/src/components/Login/Login.jsx
+++ vite-project/src/components/Login/Login.jsx
@@ -1,36 +1,61 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../AuthContext/AuthContext";
+
 export default function Login() {
+  const { login, authorization } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (authorization) {
+      navigate("home");
+    }
+  }, [authorization]);
+
+  const [user, setUser] = useState({
+    email: "",
+    contraseña: "",
+  });
+
+  function handleLogin(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
   return (
     <>
-     <div className="container">
-      <h3>Iniciar Sesion</h3>
-            <form>
-              <div className="form-group">
-                <input
-                  type="text"
-                  name="email"
-                  className="form-control mt-5"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  name="password"
-                  className="form-control mt-5"
-                  placeholder="Contraseña"
-                />
-              </div>
-              <div className="form-group">
-                <button type="submit" className="btn btn-dark mt-5">
-                  Login
-                </button>
-                <a href="#" className="btn mt-5">
-                  Forget Password?
-                </a>
-              </div>
-            </form>
-            </div>
-         
+      <div className="container">
+        <h3>Iniciar Sesion</h3>
+        <form onSubmit={(e) => login(e, user)}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="email"
+              className="form-control mt-5"
+              placeholder="Email"
+              onChange={handleLogin}
+              value={user.email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="contraseña"
+              className="form-control mt-5"
+              placeholder="Contraseña"
+              onChange={handleLogin}
+              value={user.contraseña}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-dark mt-5">
+              Login
+            </button>
+            <a href="#" className="btn mt-5">
+              Forget Password?
+            </a>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- vite-project/src/components/Registro/Registro.jsx
+++ vite-project/src/components/Registro/Registro.jsx
@@ -1,61 +1,106 @@
+import { useState, useEffect } from "react";
+import { useAuthContext } from "../../AuthContext/AuthContext";
+
 export default function Registro() {
+  const [nuevoUsuario, setNuevoUsuario] = useState({
+    email: "",
+    contraseña: "",
+    nombre: "",
+    apellidos: "",
+  });
+
+  function handleInput(e) {
+    const nuevoRegistro = {
+      ...nuevoUsuario,
+      [e.target.name]: e.target.value,
+    };
+    setNuevoUsuario(nuevoRegistro);
+  }
+
+  function registro(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(nuevoUsuario),
+    }).then((response) => {
+      console.log(response.status);
+      if (response.status === 400) {
+        alert("error al recibir el body");
+      } else if (response.status === 200) {
+        alert(`usuario ${nuevoUsuario.nombre} registrado correctamente`);
+      } else if (response.status === 409) {
+        alert("usuario ya registrado");
+      }
+    });
+    setNuevoUsuario({ email: "", contraseña: "", nombre: "", apellidos: "" });
+  }
   return (
     <>
-    <div className="container">
-      <h3>Registro</h3>
-      <form>
-        <div className="form-group mt-5">
-          <input
-            type="text"
-            className="form-control"
-            name="nombre"
-            placeholder="Nombre"
-          />
-        </div>
+      <div className="container">
+        <h3>Registro</h3>
+        <form onSubmit={registro}>
+          <div className="form-group mt-5">
+            <input
+              type="text"
+              className="form-control"
+              name="nombre"
+              placeholder="Nombre"
+              value={nuevoUsuario.nombre}
+              onChange={handleInput}
+            />
+          </div>
 
-        <div className="form-group mt-5">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Apellidos"
-            name="apellidos"
-          />
-        </div>
+          <div className="form-group mt-5">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Apellidos"
+              name="apellidos"
+              value={nuevoUsuario.apellidos}
+              onChange={handleInput}
+            />
+          </div>
 
-        <div className="form-group mt-5">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
-            name="email"
-          />
-        </div>
-        <div className="form-group mt-5">
-          <input
-            type="password"
-            className="form-control mt-5"
-            placeholder="Contraseña"
-            name="password"
-          />
-        </div>
-        <div className="form-group">
-          <input type="submit" className="btn btn-primary mt-5" />
-        </div>
-        <div className="form-check text-light mt-3">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-            required
-          />
-          <label className="form-check-label" htmlFor="flexCheckDefault">
-            Acepto la politica de privacidad
-          </label>
-        </div>
-      </form>
+          <div className="form-group mt-5">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              name="email"
+              value={nuevoUsuario.email}
+              onChange={handleInput}
+            />
+          </div>
+          <div className="form-group mt-5">
+            <input
+              type="password"
+              className="form-control mt-5"
+              placeholder="Contraseña"
+              name="contraseña"
+              value={nuevoUsuario.contraseña}
+              onChange={handleInput}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" className="btn btn-primary mt-5" />
+          </div>
+          <div className="form-check text-light mt-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+              required
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Acepto la politica de privacidad
+            </label>
+          </div>
+        </form>
       </div>
-      </>
-   
+    </>
   );
 }
