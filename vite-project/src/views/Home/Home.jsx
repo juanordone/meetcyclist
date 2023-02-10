cdf634a73b8b1abfcd7ef35fae5d0586ab4ad262
juanordone <juan.ordonez89@hotmail.es>
2023-02-10 10:40:50 +0100

    traer todas las rutas

--- vite-project/src/views/Home/Home.jsx
+++ vite-project/src/views/Home/Home.jsx
@@ -3,11 +3,31 @@ import Tarjeta from "../../components/Tarjeta/Tarjeta";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [rutas, setRutas] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/rutas");
+        const data = await response.json();
+        setRutas(data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+      
+    };
+    fetchData()
+  }, []);
   return (
     <>
       <Navbar />
       <h1>ESTO ES LA HOME </h1>
-      <Tarjeta />
+      <div className="d-flex flex-wrap">
+        {rutas.map((ruta) => (
+          <Tarjeta key={ruta.id} ciudad={ruta.ciudad} />
+        ))}
+      </div>
     </>
   );
 }
