8c809905346926feb486196c6a5794a64085136f
juanordone <juan.ordonez89@hotmail.es>
2023-02-12 21:38:16 +0100

    cambios

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../../AuthContext/AuthContext";
-import SubirRuta from "../../views/SubirRuta/SubirRuta";
+
 
 export default function Navbar() {
   const { logout, authorization } = useAuthContext();
   return (
     <>
-      <header className="p-3 mb-3 border-bottom">
-        <div className="container">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <header className="p-3 mb-3 border-bottom  ">
+        <div className="container row justify-content-evenly">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ">
             <a
               href="/"
-              className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
+              className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none fs-1"
             >
               Meet Cyclist
               <svg
@@ -23,28 +23,7 @@ export default function Navbar() {
               ></svg>
             </a>
 
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li>
-                <a href="#" className="nav-link px-2 link-secondary">
-                  Overview
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-2 link-dark">
-                  Inventory
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-2 link-dark">
-                  Customers
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-2 link-dark">
-                  Products
-                </a>
-              </li>
-            </ul>
+            
 
             <form
               className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
