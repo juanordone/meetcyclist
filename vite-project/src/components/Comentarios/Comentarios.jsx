03b0a5f8df446f2ef3c26f45fca79be0a1a3bc11
juanordone <juan.ordonez89@hotmail.es>
2023-02-15 20:11:03 +0100

    cambios

--- vite-project/src/components/Comentarios/Comentarios.jsx
+++ vite-project/src/components/Comentarios/Comentarios.jsx
@@ -6,27 +6,29 @@ export default function Comentarios() {
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
         rel="stylesheet"
       />
-      <div className="container bootdey">
-        <div className="col-md-12 bootstrap snippets">
-          <div className="panel">
-            <div className="panel-body">
-              <textarea
-                className="form-control"
-                rows="2"
-                placeholder="Que quieres saber?"
-              ></textarea>
-              <div className="mar-top clearfix">
-                <button
-                  className="btn btn-sm btn-primary pull-right"
-                  type="submit"
-                >
-                  <i className="fa fa-pencil fa-fw"></i> Publicar
-                </button>
+      <form>
+        <div className="container bootdey">
+          <div className="col-md-12 bootstrap snippets">
+            <div className="panel">
+              <div className="panel-body">
+                <textarea
+                  className="form-control"
+                  rows="2"
+                  placeholder="Que quieres saber?"
+                ></textarea>
+                <div className="mar-top clearfix">
+                  <button
+                    className="btn btn-sm btn-primary pull-right"
+                    type="submit"
+                  >
+                    <i className="fa fa-pencil fa-fw"></i> Publicar
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
