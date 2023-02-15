716b877489c8ffddd418cfc97ece71030eed46d0
juanordone <juan.ordonez89@hotmail.es>
2023-02-15 19:44:00 +0100

    cambios className

--- vite-project/src/components/Comentarios/Comentarios.jsx
+++ vite-project/src/components/Comentarios/Comentarios.jsx
@@ -6,41 +6,44 @@ export default function Comentarios() {
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
         rel="stylesheet"
       />
-      <div class="container bootdey">
-        <div class="col-md-12 bootstrap snippets">
-          <div class="panel">
-            <div class="panel-body">
+      <div className="container bootdey">
+        <div className="col-md-12 bootstrap snippets">
+          <div className="panel">
+            <div className="panel-body">
               <textarea
-                class="form-control"
+                className="form-control"
                 rows="2"
                 placeholder="Que quieres saber?"
               ></textarea>
-              <div class="mar-top clearfix">
-                <button class="btn btn-sm btn-primary pull-right" type="submit">
-                  <i class="fa fa-pencil fa-fw"></i> Publicar
+              <div className="mar-top clearfix">
+                <button
+                  className="btn btn-sm btn-primary pull-right"
+                  type="submit"
+                >
+                  <i className="fa fa-pencil fa-fw"></i> Publicar
                 </button>
               </div>
             </div>
           </div>
-          <div class="panel">
-            <div class="panel-body">
-              <div class="media-block">
-                <a class="media-left">
+          <div className="panel">
+            <div className="panel-body">
+              <div className="media-block">
+                <a className="media-left">
                   <img
-                    class="img-circle img-sm"
+                    className="img-circle img-sm"
                     alt="Profile Picture"
                     src="https://bootdey.com/img/Content/avatar/avatar1.png"
                   />
                 </a>
-                <div class="media-body">
-                  <div class="mar-btm">
+                <div className="media-body">
+                  <div className="mar-btm">
                     <a
                       href="#"
-                      class="btn-link text-semibold media-heading box-inline"
+                      className="btn-link text-semibold media-heading box-inline"
                     >
                       Lisa D.
                     </a>
-                    <p class="text-muted text-sm"> 11 min ago</p>
+                    <p className="text-muted text-sm"> 11 min ago</p>
                   </div>
                   <p></p>
                 </div>
