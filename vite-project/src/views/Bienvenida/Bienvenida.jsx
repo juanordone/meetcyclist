c1c9082d425aa6a3e357bdc5669311a4a87e02e2
juanordone <juan.ordonez89@hotmail.es>
2023-02-17 13:05:57 +0100

    diseño bienvenida

--- vite-project/src/views/Bienvenida/Bienvenida.jsx
+++ vite-project/src/views/Bienvenida/Bienvenida.jsx
@@ -8,29 +8,34 @@ export default function Bienvenida() {
   return (
     <>
       <div className="imgFondo d-flex align-content-center justify-content-center flex-wrap ">
-      <div className="position-absolute top-0 start-0 mt-3 ms-5 " id="meet">Meet Cyclist</div>
-        <div >
+        <div className="position-absolute top-0 start-0 mt-3 ms-5 " id="meet">
+          Meet Cyclist
+        </div>
+        <div>
           <div className="rounded-4 position-absolute top-0 end-0 mt-5  me-5">
             {!authorization?.email ? (
               <Boton to="iniciosesion" campo="iniciar sesion" />
             ) : (
               <Boton to="home" campo="iniciar sesion" />
             )}
-
-            
           </div>
-          <div>
-            <h1 id="titulo" > No salgas nunca mas solo </h1>
+          <div className="d-flex flex-direction-row justify-content-center">
+            <h1 className="col-8 " id="titulo">
+              {" "}
+              No salgas nunca más solo{" "}
+            </h1>
           </div>
         </div>
       </div>
-      
+
       <div className="d-flex justify-content-center">
         <section className="container-sm  row justify-content-center mt-5 ">
           <div className="col-4 ">
-            <h2 className="fs-1" id="meet">Comparte </h2>
-            <h2 className="fs-1" >Conoce </h2>
-            <h2 className="fs-1" >Disfruta </h2>
+            <h2 className="fs-1" id="meet">
+              Comparte{" "}
+            </h2>
+            <h2 className="fs-1">Conoce </h2>
+            <h2 className="fs-1">Disfruta </h2>
             <p className="mt-5">
               {" "}
               Los usuarios de Meet Cyclist publican la ruta que quieren hacer
@@ -38,7 +43,7 @@ export default function Bienvenida() {
               divertida. Olvídate de salir solo.
             </p>
             <div className="mt-5  ms-5 d-grid gap-2 col-6 mx-auto ">
-              <Boton to="iniciosesion" campo="iniciar sesion"  />
+              <Boton to="iniciosesion" campo="iniciar sesion" />
 
               <Boton to="/iniciosesion" campo="registro" />
             </div>
