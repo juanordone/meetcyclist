3be91c36f7a12c070b845f8672f1e999fd2c2517
juanordone <juan.ordonez89@hotmail.es>
2023-02-20 11:18:54 +0100

    diseño subir ruta

--- vite-project/src/views/SubirRuta/SubirRuta.jsx
+++ vite-project/src/views/SubirRuta/SubirRuta.jsx
@@ -1,13 +1,24 @@
 import Formulario from "../../components/Formulario/Formulario";
-
+import React from "react";
+import ReactPlayer from "react-player";
+import "./SubirRuta.css";
 
 export default function SubirRuta() {
   return (
     <>
-      <div className="container text-center mt-5">
-        <div className="row align-items-center">
-          <div className="col-6">
-            <Formulario />
+      <div className="imgFondo">
+        <div className="container text-center  ">
+          <div className="row align-items-center">
+            <div className="col-6 mt-5">
+              <Formulario />
+            </div>
+            <div className="col-6">
+              <ReactPlayer
+                className="react-player "
+                url="../../../tutorial.mp4"
+                controls
+              />
+            </div>
           </div>
         </div>
       </div>
