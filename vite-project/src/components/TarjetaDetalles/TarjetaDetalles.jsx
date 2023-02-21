27f88b11157bdb5adbfa0c3a4f089021683ea9f8
juanordone <juan.ordonez89@hotmail.es>
2023-02-21 19:56:05 +0100

    diseño detalles ruta

--- vite-project/src/components/TarjetaDetalles/TarjetaDetalles.jsx
+++ vite-project/src/components/TarjetaDetalles/TarjetaDetalles.jsx
@@ -1,32 +1,43 @@
-import "./TarjetaDetalle.css"
+import "./TarjetaDetalle.css";
 export default function TarjetaDetalles({ detallesRuta }) {
   return (
     <>
-      
-        <div
-          className="container d-flex justify-content-around align-items-center"
-          key={detallesRuta.id}
-        >
-          <div className="col-6 mt-5">
-            <div  dangerouslySetInnerHTML={{ __html: `${detallesRuta.url}` }} />
-          </div>
+      <div
+        className="container d-flex justify-content-around align-items-center"
+        key={detallesRuta.id}
+      >
+        <div className="col-6 mt-5">
+          <div dangerouslySetInnerHTML={{ __html: `${detallesRuta.url}` }} />
+        </div>
 
-          <div className="col-6 mt-5">
-            <p className="fs-1 fw-bold text-center ciudad ">{detallesRuta.ciudad}</p>
-            <p className="fs-3 text-center">{detallesRuta.distancia}</p>
-            <p className="fs-3 fw-bold  text-center">{detallesRuta.fecha}</p>
-            <p className="fs-3 text-center">{detallesRuta.nivel}</p>
-            <p className="fs-3 text-center">{detallesRuta.velocidad}</p>
-            <p className="fs-3 text-center">{detallesRuta.tipo}</p>
-            <p className="fs-3 text-center">{detallesRuta.duracion}</p>
-            <p className="fs-3 text-center">{detallesRuta.detalles}</p>
-
-            <div className="d-grid gap-2 p-3">
-              <button className="botonunir">Unirse a ruta</button>
+        <div className="col-6 mt-5 text-center">
+          <p className="fs-1 fw-bold  ciudad "> <i class="bi bi-geo-alt"></i> {detallesRuta.ciudad}</p>
+          <div className="d-flex justify-content-between">
+            <div className="fs-3 fw-bold text-start">
+              <p>Distancia <i class="bi bi-bicycle ciudad"></i> :</p>
+              <p>Fecha <i class="bi bi-calendar3 ciudad"></i> :</p>
+              <p>Nivel de la ruta <i class="bi bi-reception-3 ciudad"></i> :</p>
+              <p>Velocidad media <i class="bi bi-fast-forward-btn ciudad"></i> :</p>
+              <p>Tipo de ruta <i class="bi bi-repeat ciudad"></i> :</p>
+              <p>Duración aprox. <i class="bi bi-clock ciudad"></i> :</p>
+              <p>Detalles <i class="bi bi-chat-left-dots ciudad"></i> :</p>
+            </div>
+            <div className="fs-3 text-end">
+              <p>{detallesRuta.distancia}</p>
+              <p>{detallesRuta.fecha}</p>
+              <p>{detallesRuta.nivel}</p>
+              <p>{detallesRuta.velocidad}</p>
+              <p>{detallesRuta.tipo}</p>
+              <p>{detallesRuta.duracion}</p>
+              <p>{detallesRuta.detalles}</p>
             </div>
           </div>
+
+          <div className="d-grid gap-2 p-3">
+            <button className="botonunir">Unirse a ruta</button>
+          </div>
         </div>
-      
+      </div>
     </>
   );
 }
