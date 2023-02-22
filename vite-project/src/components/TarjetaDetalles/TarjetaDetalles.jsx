b69d6b70359feaf5a3212c1b0104522c77893d3a
juanordone <juan.ordonez89@hotmail.es>
2023-02-22 14:17:14 +0100

    funcionamiento del boton de unirse a ruta

--- vite-project/src/components/TarjetaDetalles/TarjetaDetalles.jsx
+++ vite-project/src/components/TarjetaDetalles/TarjetaDetalles.jsx
@@ -1,5 +1,27 @@
 import "./TarjetaDetalle.css";
+import { useParams } from "react-router-dom";
+import { useAuthContext } from "../../AuthContext/AuthContext";
 export default function TarjetaDetalles({ detallesRuta }) {
+  const { id } = useParams();
+   const {authorization} = useAuthContext()
+  function unirseRuta() {
+    fetch(`http://localhost:3000/user/addUserRuta/${detallesRuta.id}/${authorization.id}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(),
+    }).then((response) => {
+      console.log(response.status);
+      if (response.status === 400) {
+        alert("error al recibir el body");
+      } else if (response.status === 200) {
+        alert("te ha unido a la ruta");
+      } else if (response.status === 409) {
+        alert("ya estas en la ruta");
+      }
+    });
+  }
   return (
     <>
       <div
@@ -11,16 +33,34 @@ export default function TarjetaDetalles({ detallesRuta }) {
         </div>
 
         <div className="col-6 mt-5 text-center">
-          <p className="fs-1 fw-bold  ciudad "> <i class="bi bi-geo-alt"></i> {detallesRuta.ciudad}</p>
+          <p className="fs-1 fw-bold  ciudad ">
+            {" "}
+            <i className="bi bi-geo-alt"></i> {detallesRuta.ciudad}
+          </p>
           <div className="d-flex justify-content-between">
             <div className="fs-3 fw-bold text-start">
-              <p>Distancia <i class="bi bi-bicycle ciudad"></i> :</p>
-              <p>Fecha <i class="bi bi-calendar3 ciudad"></i> :</p>
-              <p>Nivel de la ruta <i class="bi bi-reception-3 ciudad"></i> :</p>
-              <p>Velocidad media <i class="bi bi-fast-forward-btn ciudad"></i> :</p>
-              <p>Tipo de ruta <i class="bi bi-repeat ciudad"></i> :</p>
-              <p>Duración aprox. <i class="bi bi-clock ciudad"></i> :</p>
-              <p>Detalles <i class="bi bi-chat-left-dots ciudad"></i> :</p>
+              <p>
+                Distancia <i className="bi bi-bicycle ciudad"></i> :
+              </p>
+              <p>
+                Fecha <i className="bi bi-calendar3 ciudad"></i> :
+              </p>
+              <p>
+                Nivel de la ruta <i className="bi bi-reception-3 ciudad"></i> :
+              </p>
+              <p>
+                Velocidad media{" "}
+                <i className="bi bi-fast-forward-btn ciudad"></i> :
+              </p>
+              <p>
+                Tipo de ruta <i className="bi bi-repeat ciudad"></i> :
+              </p>
+              <p>
+                Duración aprox. <i className="bi bi-clock ciudad"></i> :
+              </p>
+              <p>
+                Detalles <i className="bi bi-chat-left-dots ciudad"></i> :
+              </p>
             </div>
             <div className="fs-3 text-end">
               <p>{detallesRuta.distancia}</p>
@@ -34,7 +74,9 @@ export default function TarjetaDetalles({ detallesRuta }) {
           </div>
 
           <div className="d-grid gap-2 p-3">
-            <button className="botonunir">Unirse a ruta</button>
+            <button type="button" className="botonunir" onClick={unirseRuta}>
+              Unirse a ruta
+            </button>
           </div>
         </div>
       </div>
