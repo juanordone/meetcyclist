541ca5a40560b8dd38bee2849d1a554daad8526c
juanordone <juan.ordonez89@hotmail.es>
2023-02-24 13:56:05 +0100

    diseño

--- vite-project/src/components/Tarjeta/Tarjeta.jsx
+++ vite-project/src/components/Tarjeta/Tarjeta.jsx
@@ -5,10 +5,10 @@ export default function Tarjeta({ ciudad, id, url , fecha}) {
   return (
     <>
       <div className=" mt-5">
-        <div className="col-11 mb-4">
+        <div className="col-12 mb-4">
           <div className="card shadow-sm bordetarjeta p-1">
             <div
-              className="card-img d-flex justify-content-center  " 
+              className="card-img d-flex justify-content-center " 
               dangerouslySetInnerHTML={{ __html: `${url}` }}
             />
             <div className="card-body">
