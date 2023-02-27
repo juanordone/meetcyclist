dd4b45e47fa5b46ce6d3e7ed8fbb040106678bf9
juanordone <juan.ordonez89@hotmail.es>
2023-02-27 10:43:25 +0100

    responsive bienvenida

--- vite-project/src/views/Bienvenida/Bienvenida.jsx
+++ vite-project/src/views/Bienvenida/Bienvenida.jsx
@@ -7,31 +7,27 @@ export default function Bienvenida() {
   const { authorization } = useAuthContext();
   return (
     <>
-      <div className="imgFondo d-flex align-content-center justify-content-center flex-wrap ">
-        <div className="position-absolute top-0 start-0 mt-3 ms-5 " id="meet">
-          
-          Meet Cyclist
-        </div>
-        <div>
-          <div className="rounded-4 position-absolute top-0 end-0 mt-5  me-5">
-            {!authorization?.email ? (
-              <Boton to="iniciosesion" campo="iniciar sesion" />
-            ) : (
-              <Boton to="home" campo="iniciar sesion" />
-            )}
-          </div>
-          <div className="d-flex flex-direction-row justify-content-center">
-            <h1 className="col-8 " id="titulo">
-              {" "}
-              No salgas nunca más solo{" "}
-            </h1>
-          </div>
-        </div>
-      </div>
+      <div className="imgFondo d-flex align-items-center justify-content-center flex-wrap">
+  <div className="position-absolute top-0 start-0 mt-3 ms-5" id="meet">
+    Meet Cyclist <i className="bi bi-r-circle fs-6"></i>
+  </div>
+  <div className="d-flex flex-column align-items-center justify-content-center">
+    <h1 className="col-12 col-md-8 text-center" id="titulo">
+      No salgas nunca más solo
+    </h1>
+    <div className="rounded-4 position-absolute top-0 end-0 mb-3 me-5 mt-5 d-flex justify-content-end">
+      {!authorization?.email ? (
+        <Boton to="iniciosesion" campo="iniciar sesion" />
+      ) : (
+        <Boton to="home" campo="iniciar sesion" />
+      )}
+    </div>
+  </div>
+</div>
 
       <div className="d-flex justify-content-center">
         <section className="container-sm  row justify-content-center mt-5 ">
-          <div className="col-4 ">
+          <div className=" col-12 col-md-4 ">
             <h2 className="fs-1" id="meet">
               Comparte{" "}
             </h2>
@@ -43,13 +39,13 @@ export default function Bienvenida() {
               para que otros ciclistas se les unan y hacer la ruta mas amena y
               divertida. Olvídate de salir solo.
             </p>
-            <div className="mt-5  ms-5 d-grid gap-2 col-6 mx-auto ">
+            <div className="mt-5  ms-md-5 d-grid gap-2 col-8 col-md-6 mx-auto ">
               <Boton to="iniciosesion" campo="iniciar sesion" />
 
               <Boton to="/iniciosesion" campo="registro" />
             </div>
           </div>
-          <div className="col-4">
+          <div className="col-12 col-md-4">
             <img
               src="../../../bienvenida2.jpg"
               className="img-fluid"
