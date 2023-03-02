674f6510c3502c5f0554e11130204e3ba9af90c9
juanordone <juan.ordonez89@hotmail.es>
2023-03-02 12:34:01 +0100

    enlaces footer

--- vite-project/src/components/Footer/Footer.jsx
+++ vite-project/src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
-import "./Footer.css"
+import "./Footer.css";
 
 export default function Footer() {
   return (
     <>
       <footer className="text-center  ">
         <div className="container pt-4">
-          <h3 className="subeRuta">Meet Cyclist <i className="bi bi-r-circle fs-6"></i></h3>
+          <h3 className="subeRuta">
+            Meet Cyclist <i className="bi bi-r-circle fs-6"></i>
+          </h3>
           <div className="mb-4">
             <a
               className="btn btn-link btn-floating btn-lg text-dark m-1"
@@ -21,18 +23,26 @@ export default function Footer() {
               <i className="bi bi-twitter"></i>
             </a>
 
-           
-
             <a
               className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
+              href="https://www.instagram.com/juam89op/?hl=es"
+              target={"_blank"}
             >
               <i className="bi bi-instagram"></i>
             </a>
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="https://www.linkedin.com/in/juan-ord%C3%B3%C3%B1ez-p%C3%A9rez-8b9455120/"
+              target={"_blank"}
+            >
+              <i class="bi bi-linkedin"></i>
+            </a>
 
-          
-
-            <a className="btn   btn-lg  m-1" href="#!">
+            <a
+              className="btn   btn-lg  m-1"
+              href="https://github.com/juanordone"
+              target={"_blank"}
+            >
               <i className="bi bi-github"></i>
             </a>
           </div>
